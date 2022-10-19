56abae4d22e8b3682475c43f8ced9ed5590cc2d6
Alexander <canuckvisa@gmail.com>
2022-10-20 02:11:10 +0400

    ship movement test impl

--- Assets/Scripts/Game/TestInput.cs
+++ Assets/Scripts/Game/TestInput.cs
@@ -6,16 +6,25 @@ namespace Game {
 
         [SerializeField] Transform _world;
         [SerializeField] float _rotationCoefficient;
-        
+        [SerializeField] Transform _ship;
+
+
+        Vector3 _left = new Vector3(0, -10, -10);
+        Vector3 _right = new Vector3(0, 10, 10);
         void Update() {
+            
+            _ship.rotation = Quaternion.Euler(Vector3.zero);
+            
             if (Input.GetKey(KeyCode.A)) {
                 //turn left
                 _world.Rotate(Vector3.up, _rotationCoefficient);
+                _ship.rotation = Quaternion.Euler(_left);
             }
 
             if (Input.GetKey(KeyCode.D)) {
                 //turn right
                 _world.Rotate(Vector3.up, -_rotationCoefficient);
+                _ship.rotation = Quaternion.Euler(_right);
             }
         }
         
