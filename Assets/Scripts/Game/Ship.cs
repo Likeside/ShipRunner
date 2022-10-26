729d245f451b3f76cceaddc6c3f6d08143751a9a
Alexander <canuckvisa@gmail.com>
2022-10-27 01:49:07 +0400

    collectables impl prgrs

--- Assets/Scripts/Game/Ship.cs
+++ Assets/Scripts/Game/Ship.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 namespace Game {
     public class Ship: MonoBehaviour {
         [SerializeField] float _rotationMultiplier;
+
+
+        public event Action OnCollidedWithObstacle;
+        
         Vector3 _rotation = Vector3.zero;
         void Update() {
             _rotation.y = -SteeringWheel.steeringInput*_rotationMultiplier;
             _rotation.z = SteeringWheel.steeringInput*_rotationMultiplier;
             transform.rotation = Quaternion.Euler(_rotation);
+        }
+
+
+        void OnCollisionEnter(Collision other) {
+            if (other.transform.TryGetComponent(out ICollectable collectable) ) {
+                collectable.Collect();
+            }
+
+            if (other.transform.TryGetComponent(out Obstacle obstacle)) {
+                OnCollidedWithObstacle?.Invoke();
+            }
         }
     }
 }
