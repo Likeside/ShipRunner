ff14ac925c9cfa710c4029d1b6f4355b57a317ba
Alexander <canuckvisa@gmail.com>
2023-01-03 22:40:48 +0400

    zenject implemented

--- Assets/Scripts/GameNew/GameInstaller.cs
+++ Assets/Scripts/GameNew/GameInstaller.cs
@@ -1,13 +1,15 @@
 using Game;
+using UnityEngine;
 using Zenject;
 
 namespace GameNew {
     public class GameInstaller: MonoInstaller {
-        
-        
+
+        [SerializeField] InputController _inputController;
+        [SerializeField] GameplayConfig _gameplayConfig;
         public override void InstallBindings() {
-            Container.Bind<IInputController>().To<InputController>().AsSingle();
-            Container.Bind<IGameplayConfig>().To<GameplayConfig>().AsSingle();
+            Container.Bind<IInputController>().To<InputController>().FromInstance(_inputController);
+            Container.Bind<IGameplayConfig>().To<GameplayConfig>().FromInstance(_gameplayConfig);
         }
     }
 }
