5b000568e17df29d7c5ab933129cfb113f9c2e52
Alexander <canuckvisa@gmail.com>
2023-01-05 00:28:25 +0400

    sections settings

---
+++ Assets/Scripts/GameNew/Enums/PossibleSections.cs
@@ -0,0 +1,23 @@
+namespace Game {
+    public enum PossibleSections {
+        LeftStraight,
+        LeftLeft,
+        RightStraight,
+        RightRight,
+        StraightStraight,
+        StraightLeft,
+        StraightRight,
+        
+        CenterCenterBig,
+        CenterCenterBig2,
+        CenterLeftBig,
+        CenterRightBig,
+        LeftCenter,
+        RightCenter,
+        RightCenterBig,
+        RightCenterBig2,
+        StraightSmall,
+        RightLeft,
+        LeftRight
+    }
+}
